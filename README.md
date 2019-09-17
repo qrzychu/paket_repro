23a113aaa095db158ea89888e5cdeb894e0f15d4
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2019-09-17 11:11:37 +0200

    Init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Repoduction steps
+
+cd Source
+dotnet build
+dotnet pack
+
+# Fix
+
+* remove global.json
+* rerun pack
+* it works
---
+++ Source/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace paket_repro
+{
+    public class Class1
+    {
+        public void DoStuff()
+        {
+            Observable.Return(5).Subscribe(x => Console.WriteLine(x));
+        }
+    }
+}
